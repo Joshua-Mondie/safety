4dba032036951a650030e323d625d98198701668
Joshua-Mondie <joshuamondie@gmail.com>
2024-01-16 17:30:19 +0100

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
 import "./App.css";
-import AboutPages from "./pages/AboutPages";
-import BranchesPage from "./pages/BranchesPage";
-// import Header from "./pages/Header";
+// import AboutPages from "./pages/AboutPages";
+// import BranchesPage from "./pages/BranchesPage";
 import HomePage from "./pages/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NyscProgramme from "./pages/NyscProgramme";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import NyscProgramme from "./pages/NyscProgramme";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <>
-          <Route path="/home" element={<HomePage />}></Route>
-          {/* <Route path="/HomePage" element={<HomePage />}></Route> */}
-          <Route path="/branches" element={<BranchesPage />}></Route>
-          <Route path="/about" element={<AboutPages />}></Route>
-          <Route
-            path="/traning&certification"
-            element={<NyscProgramme />}
-          ></Route>
-          <h1>hello</h1>
-          {/* <Route path="landingpage">
-          
+    <>
+      <div><HomePage/></div>
+    </>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <>
+    //       <Route path="/home" element={<HomePage />}></Route>
+    //       <Route path="/branches" element={<BranchesPage />}></Route>
+    //       <Route path="/about" element={<AboutPages />}></Route>
+    //       <Route
+    //         path="/traning&certification"
+    //         element={<NyscProgramme />}
+    //       ></Route>
+    //       <h1>hello</h1>
 
-          </Route> */}
-        </>
-      </Routes>
-    </BrowserRouter>
+    //     </>
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
